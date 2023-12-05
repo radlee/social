8cba40ac95c3c9a63f8e53605a3e4f982ce2165c
radlee <prof.lolito@gmail.com>
2023-12-05 23:23:21 +0200

    Styled Topbar

---
+++ SocialMedia-Starter/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='Home'>
+        <div className='profileSide'>Profile</div>
+        <div className='postSide'>Posts</div>
+        <div className='RightSide'>Right Side</div>
+
+    </div>
+  )
+}
+
+export default Home
